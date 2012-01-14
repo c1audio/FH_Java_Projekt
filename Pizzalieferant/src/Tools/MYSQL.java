5aa8bc8f1251e5c146fcb7d018376faef375dd8b
Darkan <winters@ist-einmalig.de>
2012-01-14 16:42:24 +0100

    SQL-Interface verbessert.

--- Pizzalieferant/src/Tools/MYSQL.java
+++ Pizzalieferant/src/Tools/MYSQL.java
@@ -7,6 +7,28 @@
  * Um die Resultate zu verwerten muss die Größe der Liste mit name.getSize(); ermittelt werden.
  * Danach kann durch das Durchlaufen einer Schleife jeder einzelner Datensatz betrachtet werden.
  * 
+ * Bitte beachtet, dass in der Liste String-Arrays enthalten sind.
+ * Welche auch dementsprechend angesprochen werden müssen.
+ * 
+ * ### Beispiel:
+ * 
+ * MYSQL sqlHandling = new MYSQL();
+ * 
+ * ArrayList<String[]> results = sqlHandling.request("SELECT `id`,`Name`, `Vorname` FROM pizza.kunde");
+ * 
+ * if (result==null)
+ * {
+ * 		// FEHLER BEI ABFRAGE
+ * }
+ * 
+ * for (int id=0;id<results.size();id++)
+ * {
+ * 		System.out.println(sqlHandling.Star_to_St(results.get(id))); // Falls ihr die komplette Array in ein String packen wollt.
+ * 		System.out.println(results.get(id)[0]); // Falls ihr, wie in diesem Fall, nur die ID Ausgeben wollt.
+ * }
+ * 
+ * ### Ende
+ * 
  * @author Dennis Natanzon
  * @version 0.4
  */
@@ -23,16 +45,16 @@ public class MYSQL
 {
 	
 /** 
- * ArrayList<String> request(String Statement)
+ * ArrayList<String[]> request(String Statement)
  * 
  * @category getter 
  * @param	statement Hier gibt man die Vollständige SQL-Abfrage ein! (Bsp: "SELECT * FROM Test");
  * @return	null oder eine ArrayList vom Typ String mit allen Resultaten der Abfrage.
  */
 	
-	public ArrayList<String> request(String Statement) 
+	public ArrayList<String[]> request (String Statement) 
 	{
-		ArrayList<String> results = new ArrayList<String>();
+		ArrayList<String[]> results = new ArrayList<String[]>();
 		
 		try 
 		{
@@ -45,11 +67,10 @@ public class MYSQL
 
 			while (result.next())
 			{
-				String row = "";
-				
+				String row[] = new String[columnCount];
 				for (int colid=0; colid<columnCount;colid++)
 				{
-					row += result.getString(colid+1)+" ";
+					row[colid] = result.getString(colid+1);
 				}
 				results.add(row);
 			}
@@ -61,5 +82,26 @@ public class MYSQL
 			return null; // Fehler aufgetreten.
 		}
 		return results; // Erfolgreich ausgeführt. Rückgabe der Liste.
+	}
+	
+	/**
+	 * Umwandler von String-Array in String.
+	 * 
+	 * @param arg - Übergebenes Argument muss ein String-Array sein.
+	 * @return cache - Zurückgegebener Wert ist ein String mit den Werten des String-Arrays.
+	 */
+	
+	public String Star_to_St (String[] arg)
+	{
+		
+		int arrayl = arg.length;
+		String cache = "";
+		
+		for (int id=0; id<arrayl; id++)
+		{
+			cache += arg[id]+" ";
+		}
+		
+		return cache;
 	}
 }
